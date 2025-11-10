9f75666a1e864ef1952b2329d58a5297292d79a7
MdAsraful56 <work.mdasraful56@gmail.com>
2025-11-10 22:17:18 +0600

    cancelUnpaidAppointments

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import cron from 'node-cron';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import { AppointmentService } from './app/modules/appointment/appointment.service';
 import { PaymentController } from './app/modules/payment/payment.controller';
 import router from './app/routes';
 
@@ -25,6 +27,15 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+cron.schedule('* * * * *', () => {
+    try {
+        console.log('Node cron called at ', new Date());
+        AppointmentService.cancelUnpaidAppointments();
+    } catch (err) {
+        console.error(err);
+    }
+});
 
 app.use('/api/v1', router);
 
